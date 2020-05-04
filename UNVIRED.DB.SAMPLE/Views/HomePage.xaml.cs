55cd978b841c1a48128f58117e2127da5a6b052c
NAVEEN A M <64402819+naveenaam@users.noreply.github.com>
2020-05-04 18:42:03 +0530

    Added Setting Page

--- UNVIRED.DB.SAMPLE/Views/HomePage.xaml.cs
+++ UNVIRED.DB.SAMPLE/Views/HomePage.xaml.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Unvired.Kernel.Utils;
+using Unvired.Kernel.UWP.Log;
+using Unvired.Kernel.UWP.Login;
+using UNVIRED_REST_SAMPLE.Utility;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +31,68 @@ namespace UNVIRED.DB.SAMPLE.Views
         public HomePage()
         {
             this.InitializeComponent();
+            ApplicationVersion.Text = LoginParameters.AssemblyVersion;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void SettingButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingSplitView.IsPaneOpen = true;
+        }
+
+        private void EmailBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.SendLogViaEmail();
+        }
+
+        private void SendLogToServerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.SendLogToServer();
+        }
+
+        private void ViewLogBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.GetLogViewer();
+        }
+
+        private async void ClearLogsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var clearLogConfirmationDialog = Util.CommonContentDialog(Util.GetString("Alert"), "This will clear all the Logs associated with this application. Do you want to clear log", "Yes", "Cancel");
+                var clearLogResult = await clearLogConfirmationDialog.ShowAsync();
+
+                if (clearLogResult == ContentDialogResult.Primary)
+                {
+                    Logger.DeleteLogs();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.E($"Exception caught while clearing the application logs. Message {ex.Message}");
+            }
+        }
+
+        private async void ClearDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var clearDataConfirmationDialog = Util.CommonContentDialog("Clear Application Data", "This will clear all application related data. Are you sure you want to continue?", Util.GetString("Yes"), Util.GetString("Cancel"));
+                var clearDataResult = await clearDataConfirmationDialog.ShowAsync();
+                if (clearDataResult == ContentDialogResult.Primary)
+                {
+                    Task clearData = FrameworkHelper.ClearData();
+                    await clearData;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.E($"Exception caught while clearing the application data. Message {ex.Message}");
+            }
         }
     }
 }
